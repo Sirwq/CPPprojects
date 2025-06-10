e8a42d1b2a9a74e8ac07235b95b3a4a8913d62dd
Sirwq <vitaslav73@gmail.com>
2025-06-10 21:13:30 +0300

    little fix

--- phonebook/Person.hpp
+++ phonebook/Person.hpp
@@ -10,11 +10,11 @@ class Person {
     char lastname[20];
     char login[20];
     char adress[40];
-
 public:
     Person();
     Person(const char* name, char* lastname, char* login, char* adress);
 
+    static void inputField(const char* prompt, char* dest, size_t size);
     void print(std::ostream& os) const;
     static void createPers(Person& pers);
 };
